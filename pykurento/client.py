27dd4ef2b3e20761dfd1fe834a28781bcc1f4ee8
Gene Hallman <gene@minervaproject.com>
2014-11-27 00:59:01 -0800

    make transport pluggable

--- pykurento/client.py
+++ pykurento/client.py
@@ -2,9 +2,9 @@ from pykurento import media
 from pykurento.transport import KurentoTransport
 
 class KurentoClient(object):
-  def __init__(self, url):
+  def __init__(self, url, transport=None):
     self.url = url
-    self.transport = KurentoTransport(self.url)
+    self.transport = transport or KurentoTransport(self.url)
 
   def get_transport(self):
     return self.transport
