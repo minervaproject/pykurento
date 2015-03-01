875306183fd7624ca9cf49b948a3e328a4c5614c
Gene Hallman <gene@minervaproject.com>
2015-02-28 22:32:45 -0800

    modified the setup.py to make pypi package work

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ from setuptools import setup
 setup(name='pykurento',
       version='0.0.1',
       description='A python kurento client based on websocket-client',
-      url='http://github.com/minervaproject/pykurento',
-      author='MinervaProject',
-      author_email='dev@minervaproject.com',
+      url='https://github.com/minervaproject/pykurento',
+      download_url='https://github.com/minervaproject/pykurento/tarball/0.0.1',
+      author='Gene Hallman, MinervaProject',
+      author_email='gene@minervaproject.com',
       license='LGPL',
       packages=['pykurento'],
       install_requires=[
